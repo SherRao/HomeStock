ed24c7a5851796d4189b6cbd42035a079599660e
SherRao <SherRao15@gmail.com>
2020-05-24 12:24:18 -0400

    Fixed settings buttons event listener.

--- app/src/main/java/me/sherrao/openhacks2020/pages/SettingsFragment.java
+++ app/src/main/java/me/sherrao/openhacks2020/pages/SettingsFragment.java
@@ -1,10 +1,13 @@
 package me.sherrao.openhacks2020.pages;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import backend.Backend;
@@ -30,6 +33,30 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.pages_fragment_settings, container, false);
         app = (MainActivity) super.getActivity();
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                app.notSupportedPopup(v);
+
+            }
+        };
+
+        ImageButton accountInfoButton = root.findViewById(R.id.account_info);
+        accountInfoButton.setOnClickListener(listener);
+
+        ImageButton personalisationButton = root.findViewById(R.id.personalisation);
+        personalisationButton.setOnClickListener(listener);
+
+        ImageButton measurementButton = root.findViewById(R.id.measurements);
+        measurementButton.setOnClickListener(listener);
+
+        ImageButton langsButton = root.findViewById(R.id.langs);
+        langsButton.setOnClickListener(listener);
+
+        ImageButton helpButton = root.findViewById(R.id.help);
+        helpButton.setOnClickListener(listener);
 
         return root;
     }
